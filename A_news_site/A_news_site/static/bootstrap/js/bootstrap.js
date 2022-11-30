42b4690792c7244c62f47c98d3b30082c4b3266a
AlexFromSiberia <id764g@gmail.com>
2022-11-30 11:36:38 +0700

    Done: - templates and news_app/views corrected - Added bootstrap and css files

--- A_news_site/A_news_site/static/bootstrap/js/bootstrap.js
+++ A_news_site/A_news_site/static/bootstrap/js/bootstrap.js
@@ -193,7 +193,6 @@
     triggerTransitionEnd: function triggerTransitionEnd(element) {
       $(element).trigger(TRANSITION_END);
     },
-    // TODO: Remove in v5
     supportsTransitionEnd: function supportsTransitionEnd() {
       return Boolean(TRANSITION_END);
     },
